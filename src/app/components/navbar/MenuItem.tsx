8d74e6fbf8f87f7c6e8942c51ca01ecb2242dc6d
André Alvim <andre@alvim.net>
2023-05-20 02:04:06 -0300

    feat: UserMenu navbar

---
+++ src/app/components/navbar/MenuItem.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+interface MenuItemProps { 
+  onClick: () => void;
+  label: string;
+}
+
+
+export const MenuItem = ({ onClick, label }: MenuItemProps) => {
+  
+  return (
+    <div onClick={onClick} className="px-4 py-3 hover:bg-neutral-100 font-semibold">
+      {label}
+    </div>
+  )
+}
